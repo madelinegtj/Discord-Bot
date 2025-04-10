c58497400a0381d2e194dd61db74382173f144a6
madeline tjawardi <madelinegt16@gmail.com>
2025-04-10 23:58:32 +1000

    update

--- bot.py
+++ bot.py
@@ -1,8 +1,20 @@
 import discord
 
+# MyClient class is created by extending from the discord.Client (see discord API)
+# and will be used to interact with Discord API
 class MyClient(discord.Client):
+
+  # once Discord bot's login is successful
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
+
+  # method for bot to read message on the discord channel and respond to them
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+
+    if message.content.startswith('$hello'):
+      await message.channel.send('Hello World!')
 
 intents = discord.Intents.default()
 intents.message_content = True
